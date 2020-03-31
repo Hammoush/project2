170cfa308a19598419e62a26a65a6a1a26111acd
unknown <mhammad1991h@gmail.com>
2020-03-31 11:52:52 +0200

    opdracht(n3)

---
+++ eerste.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>home page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="continer">
+		<div class="menu">
+			<ul>
+				<li><a href="#">home</a></li>
+				<li><a href="register.php">accounts</a></li>
+				<li><a href="#">evenementen</a></li>
+				<li><a href="#">contact</a></li>
+			</ul>
+		</div>
+		<div class="content"><img src="foto/img.png"></div>
+		<div class="footer">copyright 2019</div>
+	</div>
+
+</body>
+</html>
